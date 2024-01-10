e23dba61aa8b1c9876a13c8892a04218265d7d60
Jithendra-Bathala236 <2023.jithendrab@isu.ac.in>
2024-01-10 12:27:00 +0530

    connection of mongodb is done

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from pymongo import MongoClient
 import json
 
 app = Flask(__name__)
+
+uri = "mongodb+srv://jithu:1234@cluster0.isqyov3.mongodb.net/?retryWrites=true&w=majority"
+
+# client = ""
+
+try:
+    client = MongoClient(uri)['poke-api']
+    print("Successfully connected to Mongo")
+except Exception as e:
+    print(e)
+    raise e 
+
+# print(client.list_collection_names())
 
 @app.route('/')
 def hello():
